d630e52f4c89177bb97104fc79133156cbe77de8
misskecupbung <ananda.dwirahmawati313@gmail.com>
2021-06-02 15:08:27 +0700

    konversi

--- bin/dart_hello.dart
+++ bin/dart_hello.dart
@@ -38,6 +38,25 @@ void main() {
   var age = stdin.readLineSync();
   print('Halo $namaku, usia saya adalah $age tahun');
 
+  // Number
+  var number = 1;
+  var hex = 0xB2101;
+  var double2 = 1.2;
+  var pi = 3.12;
 
+  // Konversi String > int
+  var sebelas = int.parse('11');
+  print(sebelas);
 
+  // Konversi int > string
+  var sebelas1 = 11.toString();
+  print(sebelas1);
+
+  // Konversi string > double
+  var sebelas2 = double.parse('11.2');
+  print(sebelas2);
+
+  // Konversi double > string
+  var sebelas3 = 11.22122121212122121.toStringAsFixed(4);
+  print(sebelas3);
 }
