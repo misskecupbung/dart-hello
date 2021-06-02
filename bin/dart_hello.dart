ab936376a4746700f57e19b00a4471dad9546194
misskecupbung <ananda.dwirahmawati313@gmail.com>
2021-06-02 14:06:12 +0700

    2nd commit

--- bin/dart_hello.dart
+++ bin/dart_hello.dart
@@ -1,5 +1,10 @@
 import 'package:dart_hello/dart_hello.dart' as dart_hello;
 
+/// Fungsi [main] akan menampilkan dua output.
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada library [dart_hello]
 void main(List<String> arguments) {
+  // Menampilkan Hello World
   print('Hello world: ${dart_hello.calculate()}!');
+  // Mencetak perkalian
+  print('6*7 = ${dart_hello.calculate()}');
 }
