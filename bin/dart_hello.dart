574322259184ccdaff2d8cc45403d2d674b85c04
misskecupbung <ananda.dwirahmawati313@gmail.com>
2021-06-02 14:16:17 +0700

    3nd commit

--- bin/dart_hello.dart
+++ bin/dart_hello.dart
@@ -5,6 +5,16 @@ import 'package:dart_hello/dart_hello.dart' as dart_hello;
 void main(List<String> arguments) {
   // Menampilkan Hello World
   print('Hello world: ${dart_hello.calculate()}!');
+
   // Mencetak perkalian
   print('6*7 = ${dart_hello.calculate()}');
+
+  // Mencetak Greetings dengan variabel.
+  var greetings = 'Hello Dart!';
+  print(greetings);
+
+  // Mencetak usia dengan variabel
+  var myAge;
+  //myAge = 20;
+  print(myAge);
 }
